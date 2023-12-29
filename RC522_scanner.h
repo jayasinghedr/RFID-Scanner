5c63495c9fd1594aad41368f4c72d8088f7f7eff
Dhanuja Jayasinghe <dhanujarumesh@gmail.com>
2023-12-29 12:43:50 +0530

    Added RC522 initialization

---
+++ RC522_scanner.h
@@ -0,0 +1,21 @@
+#ifndef RC522_SCANNER
+#define RC522_SCANNER
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN 22
+#define SS_PIN 5
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+
+void initialise_RC522 ()
+{
+//  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();          // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+//  Serial.println("Approximate your card to the reader...");
+//  Serial.println();
+}
+
+#endif
